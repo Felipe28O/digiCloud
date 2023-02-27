3ccdec4496e16c14d4dde0f5ded3f094b2f7e540
QV-4642\Lsierra <lsierra@qvision.com.co>
2023-02-26 20:10:23 -0500

    Se añaden plantillas de spring

---
+++ src/main/java/com/digicloud/microserviceS3/MicroserviceS3Application.java
@@ -0,0 +1,13 @@
+package com.digicloud.microserviceS3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceS3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/digicloud/microserviceS3/MicroserviceS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digicloud.microserviceS3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceS3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
